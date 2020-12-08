3f14d2caf6a89ab67e7179081fed8ad9d04b82cd
zhuangzhuang-huang <cau1403090523@gmail.com>
2020-12-08 13:46:52 +0800

    react router demo practice

--- src/App.js
+++ src/App.js
@@ -4,30 +4,31 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 
 const HomePage = props => {
-  console.log(props);
   return (
     <div>
-      <button onClick={() => props.history.push('/topics')}>Topics </button>
+      <button onClick={()=>props.history.push('/topics')}>To Topics</button>
       <h1>HOME PAGE</h1>
     </div>
   );
 };
 
-const TopicsList = props => {
+const TopicsList = (props) => {
   return (
     <div>
       <h1>TOPIC LIST PAGE</h1>
       <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
+      <Link to={`${props.match.url}/15`}>TO TOPIC 15</Link>
       <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
-      <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
+
     </div>
   );
 };
 
 const TopicDetail = props => {
+  console.log(props);
   return (
     <div>
-      <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
+      <h1>TOPIC DETAIL PAGE => {props.match.params.topicId}</h1>
     </div>
   );
 };
@@ -36,10 +37,10 @@ function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/blog/asdqw/topics' component={TopicsList} />
-      <Route path='/blog/asdqw/topics/:topicId' component={TopicDetail} />
-      <Route exact path='/blog/topics' component={TopicsList} />
-      <Route path='/blog/topics/:topicId' component={TopicDetail} />
+      <Route exact path='/topics' component={TopicsList} />
+      <Route path='/topics/:topicId' component={TopicDetail} />
+      <Route exact path='/something/else/topics' component={TopicsList} />
+      <Route path='/something/else/topics/:topicId' component={TopicDetail} />
     </div>
   );
 }
